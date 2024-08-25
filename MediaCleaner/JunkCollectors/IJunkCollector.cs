202a2c0ec8c7ae68df665cf7ceb1ffce136470d1
Denis Shemanaev <denis.shemanaev@gmail.com>
2024-08-25 13:42:14 +0300

    New option to ignore old plays

--- MediaCleaner/JunkCollectors/IJunkCollector.cs
+++ MediaCleaner/JunkCollectors/IJunkCollector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using Jellyfin.Data.Entities;
@@ -7,7 +8,7 @@ namespace MediaCleaner.JunkCollectors
 {
     internal interface IJunkCollector
     {
-        List<ExpiredItem> Execute(List<User> users, IEnumerable<IExpiredItemFilter> filters, CancellationToken cancellationToken);
-        List<ExpiredItem> ExecuteNotPlayed(List<User> users, IEnumerable<IExpiredItemFilter> filters, CancellationToken cancellationToken);
+        List<ExpiredItem> Execute(List<User> users, IEnumerable<IExpiredItemFilter> filters, DateTime? startDate, CancellationToken cancellationToken);
+        List<ExpiredItem> ExecuteNotPlayed(List<User> users, IEnumerable<IExpiredItemFilter> filters, DateTime? startDate, CancellationToken cancellationToken);
     }
 }
